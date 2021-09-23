86db12aa7540c3a89c05ec2054096ec36689c1fa
amputri <amputri1542@gmail.com>
2021-09-23 19:38:19 +0700

    fix dashboard rule

--- app/src/main/java/com/lesinaja/les/base/notifikasi/Constants.kt
+++ app/src/main/java/com/lesinaja/les/base/notifikasi/Constants.kt
@@ -3,7 +3,7 @@ package com.lesinaja.les.base.notifikasi
 class Constants {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAVvAQ_jo:APA91bFfIWxvifCXW8D7jq255qhfF6svyZhYtak4g8M-6DAXKL4zyljPjX5n2uWuvZ04KVsIXZ8WnvPXeu8GqRlZte7RBGlxlxC4qN4oE0htU8dOGuqoNyTCFJtabcKFRIJsZX4xupvB"
+        const val SERVER_KEY = "SECRET_KEY"
         const val CONTENT_TYPE = "application/json"
     }
 }
